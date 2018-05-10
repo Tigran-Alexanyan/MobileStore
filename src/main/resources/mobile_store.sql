eda20365cc368fd07460ff617bc3ec89f5622a75
Karo Darbinyan <darbinyan.karo@gmail.com>
2018-05-10 20:22:26 +0400

    Update Mobile Store project

--- src/main/resources/mobile_store.sql
+++ src/main/resources/mobile_store.sql
@@ -2,7 +2,8 @@
 SQLyog Ultimate v12.2.6 (64 bit)
 MySQL - 5.7.22 : Database - mobile_store
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -12,7 +13,7 @@ MySQL - 5.7.22 : Database - mobile_store
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`mobile_store` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mobile_store` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `mobile_store`;
 
@@ -25,14 +26,18 @@ CREATE TABLE `brand` (
   `name` varchar(255) NOT NULL,
   `pic_url` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 /*Data for the table `brand` */
 
-insert  into `brand`(`id`,`name`,`pic_url`) values 
-(1,'Samsung','kljfbkdf'),
-(2,'Apple','fddfhdf'),
-(3,'Alcatel','4u6wjteh'),
+insert  into `brand`(`id`,`name`,`pic_url`) values 
+
+(1,'Samsung','kljfbkdf'),
+
+(2,'Apple','fddfhdf'),
+
+(3,'Alcatel','4u6wjteh'),
+
 (4,'Nokia','grbfs');
 
 /*Table structure for table `category` */
@@ -44,14 +49,18 @@ CREATE TABLE `category` (
   `name` varchar(255) NOT NULL,
   `pic_url` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 /*Data for the table `category` */
 
-insert  into `category`(`id`,`name`,`pic_url`) values 
-(1,'Tablet','ndfn'),
-(2,'Phone','fdbdfnbfd'),
-(3,'Norebook','rhetg'),
+insert  into `category`(`id`,`name`,`pic_url`) values 
+
+(1,'Tablet','ndfn'),
+
+(2,'Phone','fdbdfnbfd'),
+
+(3,'Norebook','rhetg'),
+
 (4,'Tv','fn');
 
 /*Table structure for table `os` */
@@ -62,13 +71,16 @@ CREATE TABLE `os` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 /*Data for the table `os` */
 
-insert  into `os`(`id`,`name`) values 
-(1,'Android'),
-(2,'Ios'),
+insert  into `os`(`id`,`name`) values 
+
+(1,'Android'),
+
+(2,'Ios'),
+
 (3,'Windows Phone');
 
 /*Table structure for table `picture` */
@@ -82,7 +94,7 @@ CREATE TABLE `picture` (
   PRIMARY KEY (`id`),
   KEY `product_id` (`product_id`),
   CONSTRAINT `picture_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `picture` */
 
@@ -103,14 +115,14 @@ CREATE TABLE `product` (
   `front_camera` int(11) NOT NULL,
   `cpu` varchar(255) NOT NULL,
   `ram` int(11) NOT NULL,
-  `sd_card_slot` enum('???','??') NOT NULL,
+  `sd_card_slot` enum('Այո','Ոչ') NOT NULL,
   `memory` int(11) NOT NULL,
   `internet` varchar(255) NOT NULL,
-  `gps` enum('???','??') NOT NULL,
-  `bluetooth` enum('???','??') NOT NULL,
-  `3g` enum('???','??') NOT NULL,
-  `4g` enum('???','??') NOT NULL,
-  `nfc` enum('???','??') NOT NULL,
+  `gps` enum('Այո','Ոչ') NOT NULL,
+  `bluetooth` enum('Այո','Ոչ') NOT NULL,
+  `3g` enum('Այո','Ոչ') NOT NULL,
+  `4g` enum('Այո','Ոչ') NOT NULL,
+  `nfc` enum('Այո','Ոչ') NOT NULL,
   `charging` enum('USB Type-C','MicroUSB') NOT NULL,
   `battery_type` varchar(255) NOT NULL,
   `battery_capasity` int(11) NOT NULL,
@@ -130,7 +142,7 @@ CREATE TABLE `product` (
   CONSTRAINT `product_ibfk_1` FOREIGN KEY (`brand_id`) REFERENCES `brand` (`id`) ON UPDATE CASCADE,
   CONSTRAINT `product_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON UPDATE CASCADE,
   CONSTRAINT `product_ibfk_3` FOREIGN KEY (`os_id`) REFERENCES `os` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 /*Data for the table `product` */
 
@@ -146,7 +158,7 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL,
   `type` enum('USER','ADMIN') NOT NULL DEFAULT 'USER',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `user` */
 
